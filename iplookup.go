a6e3c1f261887de5177d6f8c36ee966c6fc10ff2
John M <jkmancuso@gmail.com>
2024-06-16 21:58:44 -0400

    add lookup functionality not tested yet

--- iplookup.go
+++ iplookup.go
@@ -1,44 +1,135 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
+	"net/http"
+	"os"
+	"strings"
 )
 
-type ip struct {
+type ipInfo struct {
 	ipv4       string
 	reverseDNS string
 	company    string
 }
 
-func newIP(ipAddress string) (ip, error) {
+type registryResult struct {
+	Name string `json:"name"`
+}
 
-	var reverseDNS, company string
-	resultIP := ip{}
+// main function to pack all the lookup info
+func GetIPLookupInfo(ipAddress string) (ipInfo, error) {
+
+	info, err := newIPInfo(ipAddress)
+
+	if err != nil {
+		fmt.Println(err)
+		return ipInfo{}, err
+	}
+
+	// if 192 no need to do any lookups
+	if info.isLocalLAN() {
+		return info, nil
+	}
+
+	info.setReverseDNSName()
+	info.setCompanyName()
+
+	return info, nil
+}
+
+func newIPInfo(ipAddress string) (ipInfo, error) {
 
 	if result := net.ParseIP(ipAddress); result == nil {
 		fmt.Printf("%v is not a valid ip", ipAddress)
-		return ip{}, nil
+		return ipInfo{}, nil
 	}
 
-	resultIP.ipv4 = ipAddress
-
-	addresses, err := net.LookupAddr(ipAddress)
-
-	//no need to exit
-	if err != nil || len(addresses) == 0 {
-		fmt.Println(err)
-		reverseDNS = ""
-	} else {
-		reverseDNS = addresses[0]
+	resultIP := ipInfo{
+		ipv4: ipAddress,
 	}
-
-	resultIP.reverseDNS = reverseDNS
-	resultIP.company = company
 
 	return resultIP, nil
+
 }
 
-func getValueFromCache(ipAddress string) string {
-	return ipAddress
+// basically....is the first octet 192
+func (info *ipInfo) isLocalLAN() bool {
+	octets := strings.Split(info.ipv4, ".")
+
+	if octets[0] == "192" || octets[0] == "127" {
+		return true
+	}
+
+	return false
+
+}
+
+func (info *ipInfo) setReverseDNSName() {
+	var reverseDNSName string
+
+	addresses, err := net.LookupAddr(info.ipv4)
+
+	//not fatal
+	if err != nil || len(addresses) == 0 {
+		fmt.Println(err)
+		reverseDNSName = ""
+	} else {
+		reverseDNSName = addresses[0]
+	}
+
+	info.reverseDNS = reverseDNSName
+
+}
+
+func (info *ipInfo) setCompanyName() {
+	var companyName string
+
+	result, err := queryRegistry(info.ipv4)
+
+	//not fatal
+	if err != nil {
+		fmt.Println(err)
+		companyName = ""
+	} else {
+		companyName = result.Name
+	}
+
+	info.company = companyName
+}
+
+func queryRegistry(ipAddress string) (registryResult, error) {
+	loadEnv()
+
+	result := registryResult{}
+
+	baseURL := os.Getenv("ARIN_URL")
+	url := fmt.Sprintf("%s%s", baseURL, ipAddress)
+
+	fmt.Println("Pulling from arin")
+	response, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println(err)
+		return result, err
+
+	}
+	fmt.Println("success")
+
+	var buff []byte
+
+	if _, err = response.Body.Read(buff); err != nil {
+		fmt.Println(err)
+		return result, err
+	}
+
+	if err = json.Unmarshal(buff, &result); err != nil {
+		fmt.Println(err)
+		return result, err
+	}
+
+	return result, nil
+
 }
